6bfc9a1b30831f2f73d4ff5c5dfe4926d52ed4c5
zahra falah <sepidehfallah2@gmail.com>
2018-09-11 12:51:52 -0700

    setting up model

--- FirstBlog/blog/models.py
+++ FirstBlog/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# create a table by creating a class.
+class posts (models.Model):
+
+  #variable fields
+  author = models.CharField(max_length = 30) 
+  title = models.CharField(max_length=100) 
+  bodytext = models.TextField()
+  timestamp = models.DateTimeField()
+
