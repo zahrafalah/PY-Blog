74eadcc2d517782065d5d9194151dca796a0de04
zahra falah <sepidehfallah2@gmail.com>
2018-09-17 14:54:42 -0700

    requeting django server for the home.html page

--- Dev/src/pages/views.py
+++ Dev/src/pages/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs): # Sting of HTML code
+def home_view(request, *args, **kwargs): # Sting of HTML code
     print(args, kwargs)
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
