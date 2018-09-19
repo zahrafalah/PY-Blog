71b920b3ab43ae342c6098f54112c4649bfec6f3
zahra falah <sepidehfallah2@gmail.com>
2018-09-19 14:25:01 -0700

    Posting Data To DB With A Pure Django Form

--- Dev/src/app/forms.py
+++ Dev/src/app/forms.py
@@ -11,3 +11,12 @@ class PostForm(forms.ModelForm):
            'bodytext',
            'timestamp'
        ]
+
+#Form is not a model form. it is just a standard django form, that we need to declare our inputs
+class RawPostForm(forms.Form):    
+    author = forms.CharField(max_length = 30) 
+    title = forms.CharField(max_length=100) 
+    bodytext = forms.CharField()
+  
+
+    
--- Dev/src/app/views.py
+++ Dev/src/app/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render
 from .models import posts
-from .forms import PostForm
+from .forms import PostForm, RawPostForm
 # Create your views here.
 
 def post_create_view(request):
-    form = PostForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-
+    form = RawPostForm()
+    if request.method == "POST":
+        form = RawPostForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            # we pass ** to pass it as arguments
+            posts.objects.create(**form.cleaned_data)
+        else:
+            print(form.errors)
     context = {
-        'form': form
+        "form": form
     }
     return render(request, "posts/post_create.html", context)
+
+
+# def post_create_view(request):
+#     context = {}
+#     return render(request, "posts/post_create.html", context)
+
+
+# def post_create_view(request):
+#     form = PostForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "posts/post_create.html", context)
+
 
 
 def posts_detail_view(request):
@@ -27,5 +49,5 @@ def posts_detail_view(request):
     context = {
         'object': obj
     }
-    
+
     return render(request, "posts/detail.html", context)
